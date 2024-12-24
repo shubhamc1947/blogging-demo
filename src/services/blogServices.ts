1faf25c9596af28789c8507354e7e85facd34011
shubham-covrzy <shubham@covrzy.com>
2024-12-24 11:27:11 +0530

    added env file

--- src/services/blogServices.ts
+++ src/services/blogServices.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // Base API URL
-const API_URL = "https://strapi.covrzy.com/";
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Bearer token for authorization
-const AUTH_TOKEN = "Bearer a40b9a746ebc42303552a67b246a46d2fa22e62a10c0da14318f6757f3de8ab4608068d919282ea560ea2ae12d6aa6cd33bba299d3cb434e59a3326b6b0e0a4cd40ef5a6c5f190772640e4c8ab21a81b1c2e0d3a96c4185f2844e15f1cda953b97c0fb685f69677185cce0005657e427d7c55512147febc43f2434c06bd15827";
+const AUTH_TOKEN = import.meta.env.AUTH_TOKEN;
 
 // Create an Axios instance with default headers
 const axiosInstance = axios.create({
